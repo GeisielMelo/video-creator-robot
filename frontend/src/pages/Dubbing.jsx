52dd87ba92ed838243182585f96d7b713ff3d554
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-09-06 18:00:53 -0300

    feat: Created a Dubbing page with placeholder text for testing purposes.

---
+++ frontend/src/pages/Dubbing.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Dubbing = () => {
+  return (
+    <div>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+      <h1>Dubbing</h1>
+    </div>
+  );
+};
+
+
+export default Dubbing;
