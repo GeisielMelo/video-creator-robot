aa767a717206d77b7967464e09ed4ef86497b81c
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-09-08 03:04:21 -0300

    chore: changed to a JS file

---
+++ frontend/src/config/workbenchCards.js
@@ -0,0 +1,9 @@
+export const filters = [
+  {
+    title: "Ai Text Dubbing",
+    description: "Ai Text Dubbing, dubbing in more than 140 languages.",
+    buttonDescription: "Start Dubbing",
+    url: "/ai/dubbing",
+    tags: ["all", "audio creation"],
+  },
+];
