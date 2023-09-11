9b244d855f4ec73d3809d58e00c9bc4acbc58079
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-09-11 00:23:12 -0300

    feat: Implemented the following functionalities on the left menu:
    - Added new buttons for navigation within the app.
    - Added a new logo that redirects to the main page on click.

--- frontend/src/pages/Workbench.jsx
+++ frontend/src/pages/Workbench.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Profile from "../components/Profile";
+import { useNavigate } from "react-router-dom";
+import logo from "../images/logo.png";
 
 const App = styled.section`
   display: flex;
@@ -12,6 +14,9 @@ const Menu = styled.div`
   width: 100%;
   height: 100%;
   border-right: 1px solid ${(props) => props.theme.color.Navy.Lightest};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Wrapper = styled.div`
@@ -25,6 +30,7 @@ const Content = styled.div`
   flex: 1;
   overflow-y: auto;
   padding: 20px;
+  background-color: ${(props) => props.theme.color.Blue.Lightest};
 `;
 
 const Navbar = styled.div`
@@ -41,10 +47,38 @@ const Title = styled.h1`
   font-size: ${(props) => props.theme.font.size.sm};
 `;
 
+const Logo = styled.img`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  margin: 10px;
+`;
+
+const Button = styled.button`
+  width: 170px;
+  height: 40px;
+  margin: 5px 0;
+  border-radius: 8px;
+  background-color: transparent;
+  color: ${(props) => props.theme.color.Blue.default};
+  font-size: ${(props) => props.theme.font.size.sm};
+  font-weight: ${(props) => props.theme.font.weight.bold};
+  border: 1px solid ${(props) => props.theme.color.Blue.default};
+  cursor: pointer;
+  &:hover {
+    background-color: ${(props) => props.theme.color.Blue.Lightest};
+  }
+`;
+
 const Workbench = ({ children }) => {
+  const navigate = useNavigate();
   return (
     <App>
-      <Menu>Menu</Menu>
+      <Menu>
+        <Logo src={logo} onClick={() => navigate("/")} />
+        <Button onClick={() => navigate("/ai/workbench")}>Create</Button>
+        <Button onClick={() => navigate("/ai/projects")}>Projects</Button>
+      </Menu>
       <Wrapper>
         <Navbar>
           <Title>DB Digital Components Creation</Title>
