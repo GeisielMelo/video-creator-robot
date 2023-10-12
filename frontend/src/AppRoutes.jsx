757e688868e1d66f5fe47138ceda8f0e2e70a85c
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-12 17:01:40 -0300

    refactor: Updated the App routes to utilize the Auth Context.

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { AuthProvider, AuthContext } from "./context/AuthContext";
+import { Loading } from "./components/Loading";
 
 import Index from "./pages/main/Index";
 import Login from "./pages/main/Login";
@@ -11,42 +13,32 @@ import Home from "./pages/navigation/Home";
 import StandardCreation from "./pages/navigation/StandardCreation";
 import UserUpdate from "./pages/navigation/UserUpdate";
 
-
 const AppRoutes = () => {
+  const Private = ({ children }) => {
+    const { authenticated, loading } = useContext(AuthContext);
+    return loading ? <Loading /> : authenticated ? children : <Navigate to="/sign-in" />;
+  };
+
+  const Public = ({ children }) => {
+    const { authenticated, loading } = useContext(AuthContext);
+    return loading ? <Loading /> : authenticated ? <Navigate to="/" /> : children;
+  };
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login/email" element={<Login />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="/login/forgot-password" element={<RecoverPassword />} />
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<Login />} />
+      <AuthProvider>
+        <Routes>
+          <Route path="*" element={<Public><Index /></Public>} />
+          <Route path="/" element={<Public><Index /></Public>} />
+          <Route path="/sign-in" element={<Public><Login /></Public>} />
+          <Route path="/sign-up" element={<Public><Register /></Public>} />
+          <Route path="/sign-up/forgot-password" element={<Public><RecoverPassword /></Public>} />
 
-        <Route
-          path="/home"
-          element={
-            <Workbench title="Home" page="0">
-              <Home />
-            </Workbench>
-          }
-        />
-        <Route
-          path="/video-quiz"
-          element={
-            <Workbench title="Standard Creation" page="1">
-              <StandardCreation />
-            </Workbench>
-          }
-        />
-        <Route
-          path="/user-update"
-          element={
-            <Workbench title="User Update" page="2">
-              <UserUpdate />
-            </Workbench>
-          }
-        />
-      </Routes>
+          <Route path="/home" element={<Private><Workbench title="Home" page="0"><Home /></Workbench></Private>} />
+          <Route path="/video-quiz" element={<Private><Workbench title="Standard Creation" page="1"><StandardCreation /></Workbench></Private>} />
+          <Route path="/user-update" element={<Private><Workbench title="User Update" page="2"><UserUpdate /></Workbench></Private>} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
