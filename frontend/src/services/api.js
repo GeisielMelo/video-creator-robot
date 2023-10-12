1910a8875bcf8d65e1a4e64f795664a500fe02eb
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-12 16:56:44 -0300

    feat: routes added to the api module

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,3 +3,21 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:5173/",
 });
+
+// User
+export const createUser = async (name, lastName, email, password) => {
+  return api.post("/users/", { name, lastName, email, password });
+};
+
+export const fetchUserData = async (email) => {
+  return api.get(`/users/${email}/`);
+};
+
+// Sessions
+export const createSession = async (email, password) => {
+  return api.post("/sessions/", { email, password });
+};
+
+export const checkUserEmail = async (email) => {
+  return api.get(`/sessions/${email}/`);
+};
