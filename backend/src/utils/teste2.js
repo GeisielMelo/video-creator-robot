8d0126f551efec0c05d11d6ad957004ac95a3dd9
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-17 00:11:38 -0300

    tests:  testing new methods to create new quiz components

---
+++ backend/src/utils/teste2.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const gm = require("gm").subClass({ imageMagick: "7+" });
+const wrap = require('word-wrap');
+
+const inputFile = path.resolve(__dirname, "../templates/image.png");
+const outputFile = path.resolve(__dirname, "../templates/imageOut.png");
+const fontFamily = path.resolve(__dirname, "../templates/Poppins-Regular.ttf");
+
+const text = wrap('Sansão usou a mandíbula de um jumento para eliminar quantos homens?', { width: 30, cut: false });
+const textFontSize = 48;
+const color = "#2596be";
+const WIDTH = 850;  // Largura da região
+const HEIGHT = 250; // Altura da região
+
+// Calcula a largura do texto
+const textWidth = gm().fontSize(textFontSize).font(fontFamily).drawText(0, 0, text, 'Width').size().width;
+
+// Calcula a posição X para centralizar o texto
+const X = (WIDTH - textWidth) / 2;
+
+gm(WIDTH, HEIGHT, "#ffffff") // Cria uma nova imagem branca
+  .gravity('Center') // Alinha o texto ao centro
+  .fill(color) // Define a cor do texto
+  .fontSize(textFontSize) // Define o tamanho da fonte
+  .font(fontFamily) // Define a fonte
+  .drawText(X, 0, text) // Adiciona o texto
+  .write(outputFile, function (err) {
+    if (!err) console.log('done');
+  });
---
+++ backend/src/utils/teste3.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const gm = require("gm").subClass({ imageMagick: "7+" });
+
+const inputFile1 = path.resolve(__dirname, "fundo.png"); // Imagem de 1080x1920 com fundo transparente
+const inputFile2 = path.resolve(__dirname, "imagemComTexto.png"); // Imagem com texto já escrito de 850x400
+const outputFile = path.resolve(__dirname, "saida.png");
+
+gm(inputFile1)
+  .composite(inputFile2)
+  .geometry(`+115+200`) // Centraliza com uma margem de 200px do topo
+  .write(outputFile, function (err) {
+    if (!err) console.log('done');
+  });
