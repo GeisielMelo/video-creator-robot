afdb0a22f4fd159fc33a618341457bbd4b5e8e21
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-18 19:47:23 -0300

    feat: new function on folder module to delete .png files

--- backend/src/utils/folders.js
+++ backend/src/utils/folders.js
@@ -8,4 +8,21 @@ function createFolder(folderPath) {
   }
 }
 
-module.exports = { createFolder };
+async function deletePNGFiles(folderPath) {
+  try {
+    const fullFolderPath = path.resolve(__dirname, folderPath);
+    fs.readdirSync(fullFolderPath).forEach((file) => {
+      const filePath = path.join(fullFolderPath, file);
+      const fileExtension = path.extname(filePath);
+
+      if (fileExtension === ".png") {
+        fs.unlinkSync(filePath);
+        console.log(`Deleted: ${filePath}`);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = { createFolder, deletePNGFiles };
