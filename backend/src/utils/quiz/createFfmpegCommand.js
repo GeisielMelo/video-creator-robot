1386be2df382491ec96eebc8fa75b8d5c972b64f
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-20 21:19:37 -0300

    Update createFfmpegCommand.js

--- backend/src/utils/quiz/createFfmpegCommand.js
+++ backend/src/utils/quiz/createFfmpegCommand.js
@@ -10,19 +10,18 @@ function createFfmpegCommand(array, background) {
         let answerImage = element["questionWithAnswer.png"];
         let questionLength = element.questionLength;
 
-        const command = `-loop 1 -t ${questionLength} -i ${questionImage} -loop 1 -t 1 -i ${answerImage}`;
+        const command = `-loop 1 -t ${questionLength} -i ${questionImage} -loop 1 -t 2 -i ${answerImage}`;
         commands.push(command);
       });
 
       const ffmpegCommand = `ffmpeg -i ${background} ${commands.join(
         " "
-      )} -filter_complex "[1:v][2:v]concat=n=${
-        commands.length * 2
-      }:v=1:a=0[v1];[0:v][v1]overlay=shortest=1" out.mp4`;
+      )} -filter_complex "[1:v][2:v]concat=n=2:v=1:a=0[v1];[3:v][4:v]concat=n=2:v=1:a=0[v2];[5:v][6:v]concat=n=2:v=1:a=0[v3];[7:v][8:v]concat=n=2:v=1:a=0[v4];[9:v][10:v]concat=n=2:v=1:a=0[v5];[11:v][12:v]concat=n=2:v=1:a=0[v6];[v1][v2]concat=n=2:v=1:a=0[v7];[v3][v4]concat=n=2:v=1:a=0[v8];[v5][v6]concat=n=2:v=1:a=0[v9];[v7][v8]concat=n=2:v=1:a=0[v10];[v9][v10]concat=n=2:v=1:a=0[v11];[0:v][v11]overlay=shortest=1[v12]" -map "[v12]" out.mp4"
+      `;
 
       resolve(ffmpegCommand);
     } catch (error) {
-      reject(error); 
+      reject(error);
     }
   });
 }
