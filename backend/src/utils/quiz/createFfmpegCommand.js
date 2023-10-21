d6db8511917d7d05b7e78bfb53c4145a5fc560bb
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-10-21 19:24:39 -0300

    fix: fixed upstream

--- backend/src/utils/quiz/createFfmpegCommand.js
+++ backend/src/utils/quiz/createFfmpegCommand.js
@@ -16,14 +16,8 @@ function createFfmpegCommand(array, background) {
 
       const ffmpegCommand = `ffmpeg -i ${background} ${commands.join(
         " "
-<<<<<<< Updated upstream
-      )} -filter_complex "[1:v][2:v]concat=n=${
-        commands.length * 2
-      }:v=1:a=0[v1];[0:v][v1]overlay=shortest=1" out.mp4`;
-=======
       )} -filter_complex "[1:v][2:v]concat=n=2:v=1:a=0[v1];[3:v][4:v]concat=n=2:v=1:a=0[v2];[5:v][6:v]concat=n=2:v=1:a=0[v3];[7:v][8:v]concat=n=2:v=1:a=0[v4];[9:v][10:v]concat=n=2:v=1:a=0[v5];[11:v][12:v]concat=n=2:v=1:a=0[v6];[v1][v2]concat=n=2:v=1:a=0[v7];[v3][v4]concat=n=2:v=1:a=0[v8];[v5][v6]concat=n=2:v=1:a=0[v9];[v7][v8]concat=n=2:v=1:a=0[v10];[v9][v10]concat=n=2:v=1:a=0[v11];[0:v][v11]overlay=shortest=1[v12]" -map "[v12]" -c:a copy out.mp4"
       `;
->>>>>>> Stashed changes
 
       resolve(ffmpegCommand);
     } catch (error) {
