b9233c25008758b1f78f9e7fa6d64f5e2ab2a4dd
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-23 20:15:23 -0300

    feat: two new functions to fetch data from the user downloads folder

--- backend/src/utils/folders.js
+++ backend/src/utils/folders.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 async function fetchAudioQuestionsPath(userId) {
   try {
-    const questionsArray = [];
+    let questionsArray = [];
     const files = await fs.promises.readdir(path.resolve(__dirname, `../downloads/${userId}`));
 
     files.forEach((file) => {
@@ -16,8 +16,50 @@ async function fetchAudioQuestionsPath(userId) {
     });
     return questionsArray;
   } catch (error) {
-    throw error;
+    console.error(error);
   }
 }
 
-module.exports = { fetchAudioQuestionsPath };
+async function fetchQuestionsWithAlternatives(userId) {
+  try {
+    let questionsArray = [];
+    const files = await fs.promises.readdir(path.resolve(__dirname, `../downloads/${userId}`));
+
+    files.forEach((file) => {
+      const filePath = path.join(path.resolve(__dirname, `../downloads/${userId}`), file);
+      const fileExtension = path.extname(filePath);
+
+      if (fileExtension === ".png") {
+        if (file.includes("questionWithAlternatives")) {
+          questionsArray.push(filePath);
+        }
+      }
+    });
+    return questionsArray;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchQuestionsWithAnswers(userId) {
+  try {
+    let questionsArray = [];
+    const files = await fs.promises.readdir(path.resolve(__dirname, `../downloads/${userId}`));
+
+    files.forEach((file) => {
+      const filePath = path.join(path.resolve(__dirname, `../downloads/${userId}`), file);
+      const fileExtension = path.extname(filePath);
+
+      if (fileExtension === ".png") {
+        if (file.includes("questionWithAnswer")) {
+          questionsArray.push(filePath);
+        }
+      }
+    });
+    return questionsArray;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = { fetchAudioQuestionsPath, fetchQuestionsWithAlternatives, fetchQuestionsWithAnswers };
