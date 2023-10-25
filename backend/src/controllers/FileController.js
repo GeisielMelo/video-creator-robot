4b6eb9afa9dcb76aa3344c7b75bd2679af5a1bde
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-25 14:43:55 -0300

    feat: Finished modules for video and audio generation.

--- backend/src/controllers/FileController.js
+++ backend/src/controllers/FileController.js
@@ -2,8 +2,7 @@ import path from "path";
 import TextToImagesCreator from "../modules/TextToImagesCreator";
 import ImageToVideoCreator from "../modules/ImageToVideoCreator";
 import AudioConcatenator from "../modules/AudioConcatenator";
-
-//import TextToSpeechCreator from "../modules/TextToSpeechCreator"
+import TextToSpeechCreator from "../modules/TextToSpeechCreator";
 
 class FileController {
   async downloadFile(req, res) {
@@ -17,26 +16,21 @@ class FileController {
   }
 
   async createQuiz(req, res) {
-    // Todo
-    // CRIAR MÉTODO PARA DELETAR ARQUIVOS ANTERIORES.
-    // REMOVER CRIAÇÃO DE PASTAS DE TEXT TO IMAGES E ADICIONAR EM FOLDERS.
     try {
       const { userId, questions } = req.body;
 
-      // OK
+      // CRIAR MÉTODO PARA DELETAR ARQUIVOS ANTERIORES.
+      // REMOVER CRIAÇÃO DE PASTAS DE TEXT TO IMAGES E ADICIONAR EM FOLDERS.
+
       const textToImagesCreator = new TextToImagesCreator(questions, userId);
       await textToImagesCreator.render();
 
-      // OK
       const imageToVideoCreator = new ImageToVideoCreator(userId);
       await imageToVideoCreator.render();
 
-      // Todo
-      //const textToSpeechCreator = new TextToSpeechCreator(questions, userId);
-      //await textToSpeechCreator.create();
+      const textToSpeechCreator = new TextToSpeechCreator(questions, userId);
+      await textToSpeechCreator.CreateAudiosWithElevenLabs();
 
-      // OK
-      // Todo mover fetchAudioQuestionsPath para dentro da classe.
       const audioConcatenator = new AudioConcatenator(userId);
       await audioConcatenator.concatenate();
 
