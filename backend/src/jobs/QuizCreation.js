63b545aacbbefc3091f4440769b098df76fd9f0c
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-30 13:07:20 -0300

    feat: moved user folder management to a class for more control

--- backend/src/jobs/QuizCreation.js
+++ backend/src/jobs/QuizCreation.js
@@ -4,36 +4,21 @@ import ImageToVideoCreator from "../modules/ImageToVideoCreator";
 import AudioConcatenator from "../modules/AudioConcatenator";
 import TextToSpeechCreator from "../modules/TextToSpeechCreator";
 import VideoCreator from "../modules/VideoCreator";
+import FilesManagement from "../modules/FilesManagement";
 
 export default {
   key: "QuizCreation",
   async handle({ data }) {
-    const {
-      quiz: { userId, questions, solicitationNumber },
-    } = data;
+    const { userId, questions, solicitationNumber } = data.quiz;
 
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    return console.log(userId, questions, solicitationNumber);
+    try {
+      const filesManagement = new FilesManagement(userId, solicitationNumber);
+      await filesManagement.createUserFolders();
+      await filesManagement.clearProcessingFolder();
+
+      return console.log("Successfully created.");
+    } catch (error) {
+      return console.log(error);
+    }
   },
 };
-
-
-// await deleteUserFiles(userId);
-// await createFolder(`../downloads/${userId}/${solicitationNumber}`);
-
-// const textToImagesCreator = new TextToImagesCreator(questions, userId);
-// await textToImagesCreator.render();
-
-// const textToSpeechCreator = new TextToSpeechCreator(questions, userId);
-// await textToSpeechCreator.CreateAudiosWithElevenLabs();
-
-// const imageToVideoCreator = new ImageToVideoCreator(userId);
-// await imageToVideoCreator.render();
-
-// const audioConcatenator = new AudioConcatenator(userId);
-// await audioConcatenator.concatenate();
-
-// const videoCreator = new VideoCreator(userId, solicitationNumber);
-// await videoCreator.render();
-
-// await deleteUserFiles(userId);
