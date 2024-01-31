d6d99226c9d6ba12e50dc6fab5d37729e4d51a93
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-30 23:52:07 -0300

    fix: Updated _elevenLabsAudioGenerator to make the app work with the new ElevenLabs API update.

--- backend/src/modules/TextToSpeechCreator.js
+++ backend/src/modules/TextToSpeechCreator.js
@@ -9,44 +9,49 @@ class TextToSpeechCreator {
   }
 
   async _elevenLabsAudioGenerator(text, outputName) {
-    const url = "https://api.elevenlabs.io/v1/text-to-speech/pNInz6obpgDQGcFmaJgB";
+    return new Promise((resolve, reject) => {
+      const url = "https://api.elevenlabs.io/v1/text-to-speech/pNInz6obpgDQGcFmaJgB";
 
-    const headers = {
-      Accept: "audio/mpeg",
-      "Content-Type": "application/json",
-      "xi-api-key": "6b01a4f03be91c802fc31a80856a75c2",
-    };
+      const headers = {
+        Accept: "audio/mpeg",
+        "Content-Type": "application/json",
+        "xi-api-key": "YOUR ELEVENLABS API KEY HERE!",
+      };
 
-    const data = {
-      text: text,
-      model_id: "eleven_multilingual_v1",
-      voice_settings: {
-        stability: 0.5,
-        similarity_boost: 0.75,
-      },
-    };
+      const data = {
+        text: text,
+        model_id: "eleven_multilingual_v1",
+        voice_settings: {
+          stability: 0.5,
+          similarity_boost: 0.75,
+        },
+      };
 
-    return new Promise(async (resolve, reject) => {
-      try {
-        const response = await axios.post(url, data, {
-          headers,
-          responseType: "stream",
+      axios
+        .post(url, data, { headers, responseType: "stream" })
+        .then((response) => {
+          const outputAudio = path.resolve(__dirname, `../archives/${this.userId}/processing/${outputName}.mp3`);
+          const writer = fs.createWriteStream(outputAudio);
+
+          response.data.on("data", (chunk) => {
+            writer.write(chunk, "binary");
+          });
+
+          response.data.on("end", () => {
+            writer.end();
+            resolve(outputAudio); // Resolve with the path to the saved audio file
+          });
+
+          response.data.on("error", (error) => {
+            console.error("Error in response stream:", error);
+            writer.end();
+            reject(error); // Reject with the error if there is one
+          });
+        })
+        .catch((error) => {
+          console.error("Error in axios request:", error);
+          reject(error); // Reject with the error if there is one
         });
-
-        const outputAudio = path.resolve(__dirname, `../archives/${this.userId}/processing/${outputName}.mp3`);
-        const writer = fs.createWriteStream(outputAudio);
-
-        response.data.on("data", (chunk) => {
-          writer.write(chunk, "binary");
-        });
-
-        response.data.on("end", () => {
-          writer.end();
-          resolve();
-        });
-      } catch (error) {
-        reject(error);
-      }
     });
   }
 
@@ -56,10 +61,11 @@ class TextToSpeechCreator {
       for (let [index, element] of this.jsonArray.entries()) {
         await this._elevenLabsAudioGenerator(element.question, `question${index + 1}`);
       }
+
+      console.log(`Successfully ElevenLabs audios for user:${this.userId}`);
     } catch (error) {
-      console.error(`Error on generate audios with elevenLabs.`);
+      console.error(`Error on generate audios with elevenLabs.`, error);
     }
-    console.log(`Successfully ElevenLabs audios for user:${this.userId}`);
   }
 }
 
